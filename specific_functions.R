f7d6801e9ebe34c3ccec491aa6cbf6c9c586fe82
Anna <annagabriela2940@gmail.com>
2026-04-02 18:53:08 -0500

    Updates...

--- specific_functions.R
+++ specific_functions.R
@@ -8,7 +8,8 @@ file_path <- file.path(getwd(), "res-1555-2010.pdf") # mortality table path
 .mortality_file <- pdf_text(file_path)[2]
 
 ## START ##
-# price_case_11
+
+# Problem 1
 price_case_11 <- function(x,
                      m,
                      r,
@@ -71,7 +72,7 @@ price_case_09 <- function(x,
   
 }
 
-
+# Problem 2
 price_case_25 <- function(x,
                      m,
                      r,
@@ -109,8 +110,39 @@ Problem3 <- function(x,
   mortality_table <- process_mortality_table(.mortality_file, interest)
   mortality_table <- as.data.frame(mortality_table)
   
-  pos_in_table <- which(mortality_table[, "age"] == x)
+  pos_in_table1 <- which(mortality_table[, "age"] == x)
+  pos_in_table2 <- which(mortality_table[, "age"] == x+n)
   interest_m <- m * ((1 + interest)^(1 / m) - 1)
+
+  
+  if (gender == "women") {
+    result1 <- (mortality_table$Mx_women[pos_in_table1] 
+                - mortality_table$Mx_women[pos_in_table2]
+                ) / mortality_table$Dx_women[pos_in_table1]
+    result2 <- (mortality_table$Rx_women[pos_in_table1]
+                - mortality_table$Rx_women[pos_in_table2]
+                - n * mortality_table$Mx_women[pos_in_table2]
+                ) / mortality_table$Dx_women[pos_in_table1]
+    result3 <- mortality_table$Mx_women[pos_in_table2] / mortality_table$Dx_women[pos_in_table1]
+    result4 <- result2 + result3
+  } else if (gender == "men"){
+    result1 <- (mortality_table$Mx_men[pos_in_table1] 
+                - mortality_table$Mx_men[pos_in_table2]
+                ) / mortality_table$Dx_men[pos_in_table1]
+    result2 <- (mortality_table$Rx_men[pos_in_table1]
+                - mortality_table$Rx_men[pos_in_table2]
+                - n*mortality_table$Mx_men[pos_in_table2]
+                ) / mortality_table$Dx_men[pos_in_table1]
+    result3 <- mortality_table$Mx_men[pos_in_table2] / mortality_table$Dx_men[pos_in_table1]
+    result4 <- result2 + result3
+  }
+  
+  
+  result <- (interest / interest_m) * (
+    result1 + r * result4
+  )
+
+  result
 }
 
 Problem4 <- function(x,
@@ -135,5 +167,8 @@ tst1 <- price_case_11(34, 12, 0.05, .mortality_file, "women", 0.25)
 tst1
 tst2 <- price_case_25(34, 12, 0.07, .mortality_file, "women", 0.15)
 tst2
-tst3 <- price_case_09(56, 2, 0.07, .mortality_file, "women", 0.17)
+tst3 <- price_case_09(25, 12, 0.05, .mortality_file, "women", 0.1)
 tst3
+tst4 <- Problem3(25, 12, 0.05, 49, .mortality_file, "women", 0.1)
+tst4
+0.0185161
