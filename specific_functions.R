50696ff170ca3cff2525e6097f3dddac5be9502c
Anna <annagabriela2940@gmail.com>
2026-04-02 15:43:59 -0500

    FEAT: Problem2 (still not ok)

--- specific_functions.R
+++ specific_functions.R
@@ -1,22 +1,20 @@
 
 
-EnteroFraccionarioCrecimientoAritmetico <- function(x,
-                                                    m,
-                                                    r,
-                                                    mortality_table,
-                                                    gender = "M",
-                                                    interest) {
+Problem1 <- function(x,
+                     m,
+                     r,
+                     mortality_table,
+                     gender = "women",
+                     interest) {
   
   pos_in_table <- which(mortality_table[, "age"] == x)
-  
   interest_m <- m * ((1 + interest)^(1 / m) - 1)
+  
   if (gender == "women") {
-    result1 <- mortality_table$IAx_women[pos_in_table] - mortality_table$Ax_women[x -
-                                                                              14]
+    result1 <- mortality_table$IAx_women[pos_in_table] - mortality_table$Ax_women[pos_in_table]
     result2 <- mortality_table$Ax_women[pos_in_table] + r * result1
   } else if (gender == "men") {
-    result1 <- mortality_table$IAx_men[pos_in_table] - mortality_table$Ax_men[x -
-                                                                          14]
+    result1 <- mortality_table$IAx_men[pos_in_table] - mortality_table$Ax_men[pos_in_table]
     result2 <- mortality_table$Ax_men[pos_in_table] + r * result1
   }
   
@@ -25,3 +23,35 @@ EnteroFraccionarioCrecimientoAritmetico <- function(x,
   # returns
   result
 }
+
+Problem2 <- function(x,
+                     m,
+                     r,
+                     mortality_table,
+                     gender = "women",
+                     interest) {
+  pos_in_table <- which(mortality_table[, "age"] == x)
+  interest_m <- m * ((1 + interest)^(1 / m) - 1)
+  
+  if (gender == "women") {
+    result1 <- Problem1(x, m, r, mortality_table,
+                        gender, interest)
+    result2 <- mortality_table$Ax_women[pos_in_table] * r * (
+      (interest_m - interest) / interest_m^2
+    )
+  } else if (gender == "men") {
+    result1 <- Problem1(x, m, r, mortality_table,
+                        gender, interest)
+    result2 <- mortality_table$Ax_men[pos_in_table] * r * (
+      (interest_m - interest) / interest_m^2
+    )
+  }
+  result <- result1 + result2
+  return(result)
+}
+
+tst1 <- Problem1(34, 12, 0.05, mortality_table, "women", 0.1)
+tst1
+tst2 <- Problem2(34, 12, 0.05, mortality_table, "women", 0.1)
+tst2
+
