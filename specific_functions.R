8786f8ee3ffc8de04a4006c0ef18d4cb843bed9f
Anna <annagabriela2940@gmail.com>
2026-04-02 17:31:05 -0500

    Updates...

--- specific_functions.R
+++ specific_functions.R
@@ -36,7 +36,8 @@ Problem1 <- function(x,
   result
 }
 
-Problem3 <- function(x,
+
+Problem25 <- function(x,
                      m,
                      r,
                      .mortality_file,
@@ -44,13 +45,11 @@ Problem3 <- function(x,
                      interest) {
 
   mortality_table <- process_mortality_table(.mortality_file, interest)
-  # browser()
   mortality_table <- as.data.frame(mortality_table)
-  # browser()
+
   pos_in_table <- which(mortality_table[, "age"] == x)
-  # browser()
   interest_m <- m * ((1 + interest)^(1 / m) - 1)
-  # browser()
+
   if (gender == "women") {
     result1 <- Problem1(x, m, r, .mortality_file,
                         gender, interest)
@@ -66,10 +65,72 @@ Problem3 <- function(x,
   }
   
   result <- result1 + result2
-  return(result)
+  
+  result
+  
+}
+
+
+Problem2 <- function(x,
+                     m,
+                     r,
+                     .mortality_file,
+                     gender = "women",
+                     interest) {
+  mortality_table <- process_mortality_table(.mortality_file, interest)
+  mortality_table <- as.data.frame(mortality_table)
+  
+  pos_in_table <- which(mortality_table[, "age"] == x)
+  interest_m <- m * ((1 + interest)^(1 / m) - 1)
+  result1 <- Problem25(x, m, r, .mortality_file,
+                      gender, interest)
+  
+  if (gender == "women") {
+    result2 <- (r * interest * mortality_table$Ax_women[pos_in_table]) / (m * interest_m)
+  } else if (gender == "men") {
+    result2 <- (r * interest * mortality_table$Ax_men[pos_in_table]) / (m * interest_m)
+  }
+  
+  result <- result1 + result2
+  
+  result
+}
+
+
+Problem3 <- function(x,
+                     m,
+                     r,
+                     n,
+                     .mortality_file,
+                     gender = "women",
+                     interest) {
+  mortality_table <- process_mortality_table(.mortality_file, interest)
+  mortality_table <- as.data.frame(mortality_table)
+  
+  pos_in_table <- which(mortality_table[, "age"] == x)
+  interest_m <- m * ((1 + interest)^(1 / m) - 1)
+}
+
+Problem4 <- function(x,
+                     m,
+                     r,
+                     .mortality_file,
+                     gender = "women",
+                     interest) {
+  
+  mortality_table <- process_mortality_table(.mortality_file, interest)
+  mortality_table <- as.data.frame(mortality_table)
+  
+  pos_in_table <- which(mortality_table[, "age"] == x)
+  interest_m <- m * ((1 + interest)^(1 / m) - 1)
+  result1 <- Problem3(x, m, r, .mortality_file,
+                      gender, interest)
+  
 }
 
 tst1 <- Problem1(34, 12, 0.05, .mortality_file, "women", 0.25)
 tst1
 tst2 <- Problem3(34, 12, 0.07, .mortality_file, "women", 0.15)
 tst2
+tst3 <- Problem2(56, 2, 0.07, .mortality_file, "women", 0.17)
+tst3
